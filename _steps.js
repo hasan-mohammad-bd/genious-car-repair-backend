57b5895ff6411a637ca198644d2085a335b1cb01
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-04-26 12:13:40 +0600

    adding steps

---
+++ _steps.js
@@ -0,0 +1,13 @@
+/* 
+* One Time for you computer
+1. Create heroku account
+2. verify email.
+3. install heroku cli
+4. heroku login
+*--------------------------
+for each project one time
+*--------------------------
+1. check weather the node local server is running or not.
+2. heroku create
+3. 
+*/
